3fae8a470da8d097b3def35ee62dcb56fa366226
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2022-04-24 15:37:54 +0500

    set neumorphic error

--- lib/HomeScreen/Log_in.dart
+++ lib/HomeScreen/Log_in.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:siba_cms_2/HomeScreen/Home_Screen.dart';
 
 class LogIn extends StatefulWidget {
